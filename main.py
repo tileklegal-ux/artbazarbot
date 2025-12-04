4b9023b33defa3c6ee7a388137aef6d59a44a96d
tileklegal-ux <tileklegal@gmail.com>
2025-12-04 19:41:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8389875803:AAGcCO8bG1mQS2khsUa5S2BqD8J0kfc68Bo")
+# Забираем токен из переменной среды
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is running!")
+    await update.message.reply_text("Бот работает! 👋")
 
 def main():
     app = Application.builder().token(TOKEN).build()
+    
     app.add_handler(CommandHandler("start", start))
+    
     app.run_polling()
 
 if __name__ == "__main__":
