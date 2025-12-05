05e9d54f2abae385dae2eed32ed2693859472172
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 15:46:23 +0600

    Create messages_kg.py

---
+++ messages_kg.py
@@ -0,0 +1,10 @@
+texts = {
+    "start": "ArtBazar AI жардамчысына кош келиңиз!",
+    "select_language": "Тилди тандаңыз:",
+    "menu_main": "Башкы меню",
+    "button_market": "Базар анализи",
+    "button_niche": "Ниша тандоо",
+    "button_profit": "Маржа эсептегич",
+    "button_recommend": "Сунуштар",
+    "button_premium": "Премиум мүмкүнчүлүк",
+}
