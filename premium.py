151e250a40962a1850149ee90fe91e1b49eab2f8
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 16:13:36 +0600

    Update premium.py

--- premium.py
+++ premium.py
@@ -1,16 +1,12 @@
 import time
 from database import set_premium, get_user
 
+def add_premium(user_id, months):
+    until = int(time.time()) + months * 30 * 24 * 3600
+    set_premium(user_id, until)
+    return until
 
-def add_premium(user_id: int, months: int, manager_id: int):
-    expire_at = int(time.time()) + months * 30 * 24 * 3600
-    set_premium(user_id, expire_at, manager_id)
-    return expire_at
 
-
-def is_premium(user_id: int):
+def is_premium(user_id):
     user = get_user(user_id)
-    if not user or not user["premium_until"]:
-        return False
-
-    return user["premium_until"] > int(time.time())
+    return user and user["premium_until"] > int(time.time())
