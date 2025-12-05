44ec8ab035fcaedc03cc0f3ab1d3bbd882e0c180
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 15:53:15 +0600

    Update premium.py

--- premium.py
+++ premium.py
@@ -1,12 +1,16 @@
-# premium.py
-
-from database import activate_premium, user_has_premium
-
-
-def check_premium(user_id):
-    return user_has_premium(user_id)
+import time
+from database import set_premium, get_user
 
 
-def give_premium(user_id, manager_id, months):
-    activate_premium(user_id, manager_id, months)
-    return True
+def add_premium(user_id: int, months: int, manager_id: int):
+    expire_at = int(time.time()) + months * 30 * 24 * 3600
+    set_premium(user_id, expire_at, manager_id)
+    return expire_at
+
+
+def is_premium(user_id: int):
+    user = get_user(user_id)
+    if not user or not user["premium_until"]:
+        return False
+
+    return user["premium_until"] > int(time.time())
