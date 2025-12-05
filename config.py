a572c48fef7e05a3ffb901b608198b8464c9dbc9
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 15:52:40 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
-DEFAULT_LANGUAGE = "ru"
-
-SUPPORTED_LANGUAGES = ["ru", "kg", "kz"]
-
-LANG_FILES = {
-    "ru": "messages_ru",
-    "kg": "messages_kg",
-    "kz": "messages_kz",
-}
-
-# OPENAI ключ брать из переменной окружения на Fly
 import os
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 DB_PATH = "database.db"
