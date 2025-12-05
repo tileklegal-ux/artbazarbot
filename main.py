cf0215a958886fc4a3be52e9dfa14de9335cbe16
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 17:12:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
+import os
+
 from config import BOT_TOKEN
-from database import init_db
 from handlers import router
 
 async def main():
-    init_db()
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
-    await dp.start_polling(bot)
+
+    # webhook config
+    webhook_path = os.getenv("WEBHOOK_PATH")
+    webhook_url = os.getenv("WEBHOOK_URL")
+
+    # создаём Aiohttp app
+    app = web.Application()
+    SimpleRequestHandler(dp, bot).register(app, path=webhook_path)
+    setup_application(app, dp)
+
+    # устанавливаем webhook
+    await bot.set_webhook(webhook_url)
+
+    # стартуем сервер
+    port = int(os.getenv("PORT", 8080))
+    return app
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    web.run_app(main(), host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
