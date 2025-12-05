0df97965cffe9824919a3710d18a974924e41d4d
tileklegal-ux <tileklegal@gmail.com>
2025-12-05 15:46:03 +0600

    Create messages_ru.py

---
+++ messages_ru.py
@@ -0,0 +1,10 @@
+texts = {
+    "start": "Добро пожаловать в ArtBazar AI Assistant!",
+    "select_language": "Выберите язык:",
+    "menu_main": "Главное меню",
+    "button_market": "Анализ рынка",
+    "button niche": "Подбор ниши",
+    "button_profit": "Калькулятор маржи",
+    "button_recommend": "Рекомендации",
+    "button_premium": "Премиум доступ",
+}
